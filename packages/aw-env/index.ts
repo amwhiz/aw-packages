539a32408e85dc68c0c044895ce750d5814adce9
keerthikraj <keerthikmca05@gmail.com>
2023-12-22 20:28:25 +0530

    src: add logger package

--- packages/aw-env/index.ts
+++ packages/aw-env/index.ts
@@ -3,6 +3,7 @@ const Environments = Object.freeze({
   cdsUserName: process.env.CDS_AUTH_USERNAME,
   cdsPassword: process.env.CDS_AUTH_PASSWORD,
   cdsEncryptionKey: process.env.SIM_ACTIVATION_ENCRYPTION_KEY,
+  serviceName: process.env.SERVICE_NAME,
 });
 
 type KeyType = keyof typeof Environments;
