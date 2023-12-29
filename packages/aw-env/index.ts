6ec65e8823c5e50743707d348a5e70d86cd18a21
keerthikraj <keerthikmca05@gmail.com>
2023-12-29 12:54:27 +0530

    src: update dynamoDB version

--- packages/aw-env/index.ts
+++ packages/aw-env/index.ts
@@ -3,13 +3,8 @@ const Environments = Object.freeze({
   accessKey: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: process.env.REGION,
-  queueUrl: process.env.QUEUE_URL,
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET_ID,
   serviceName: process.env.SERVICE_NAME,
   enableLogger: process.env.ENABLE_LOGGER,
-  hubClientSecret: process.env.HUB_CLIENT_SECRET,
-  lambdaUri: process.env.LAMBDA_URI,
 });
 
 type KeyType = keyof typeof Environments;
