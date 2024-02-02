1e7f6d42508ba16cc0dd7257a9e421fde21e7aca
Nagaraj0229 <npnagu50@gmail.com>
2024-02-02 14:28:25 +0530

    src: update auth-package

---
+++ packages/aw-auth/refreshAccessToken/refreshAccessToken.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { env } from '@aw/env';
+import { logger } from '@aw/logger';
+import axios, { AxiosResponse } from 'axios';
+
+export class RefreshAccessToken {
+  private payload: object;
+
+  public constructor(client: object) {
+    this.payload = client;
+  }
+
+  public async refreshAccessToken(): Promise<any> {
+    const url = env('authUrl') as string;
+    const data = this.payload;
+    logger.info('RefreshAccessToken: Credential Data', { data });
+
+    try {
+      const response: AxiosResponse<any> = await axios.post(url, new URLSearchParams(data as any), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+        },
+      });
+      return response;
+    } catch (error: any) {
+      logger.error('Error refreshing access token:', error.message);
+      throw error;
+    }
+  }
+}
