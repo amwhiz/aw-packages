01665fcf765fbb94ead6570e7e59daaa8674851a
suryavi <suryaplaystore325@gmail.com>
2024-02-15 16:20:54 +0530

    chore: updated the hubspot turn off future

--- packages/aw-crm/aw-hubspot/index.ts
+++ packages/aw-crm/aw-hubspot/index.ts
@@ -10,9 +10,17 @@ export class HubspotCRM extends BaseCRMProvider {
   private _authService: Auth;
   private _contactService: Contact;
   private _signatureService: Signature;
+  // eslint-disable-next-line no-undef
+  private hubspotProviderIsTurnedOff: number = +(process.env.TURN_OFF_HUBSPOT_SERVICE || '0');
 
   constructor(config?: IConfiguration) {
     super();
+    /* The code `if (this.hubspotProviderIsTurnedOff) throw new Error('Hubspot Provider is turn off');`
+    is checking the value of the `hubspotProviderIsTurnedOff` variable. If the value is truthy (not
+    equal to 0), it means that the Hubspot provider is turned off. In this case, it throws an error
+    with the message 'Hubspot Provider is turn off'. This is a way to prevent the HubspotCRM class
+    from being instantiated when the provider is turned off. */
+    if (this.hubspotProviderIsTurnedOff) throw new Error('Hubspot Provider is turn off');
     this.client = new Client(config);
   }
 
